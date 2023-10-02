9134670244f7a7f05e4209f288fe15c2d14b675c
whoami-anoint <abhishekkfl@gmail.com>
2023-10-02 17:44:55 +0530

    update files

--- 3rd.rs
+++ 3rd.rs
@@ -1,6 +1,8 @@
 fn main(){
-    let a:i8 = 127;
+    let a:i8 = 124;
     println!("{}",a);
 }
 
-// 
+// :i8 = -128 -> 127,
+// i8 => signed (positive + negative (both))
+// u8 => unsigned (only positive)
