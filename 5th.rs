b67009a5258636a8b46a2b9a952438a27588c480
whoami-anoint <abhishekkfl@gmail.com>
2023-10-04 17:37:27 +0530

    update files

--- 5th.rs
+++ 5th.rs
@@ -1,6 +1,11 @@
 fn main(){
-    let one = 1;
-    println!("{}",one);
-    let two: i8 = 12;
-    println!("{}",two);
+    // let one = 1;
+    // let two: i8 = 2;
+    // let three = 3i8;
+    // let four = 4_i8;
+    // println!("{}",one);
+    // println!("{}",two);
+    // println!("{}",three);
+    // println!("{}",four);
+
 }
