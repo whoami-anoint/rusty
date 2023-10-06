2c240fb642d7ec01f87d1c1312ee51e42585d613
whoami-anoint <abhishekkfl@gmail.com>
2023-10-06 22:43:14 +0530

    update files

--- 8th.rs
+++ 8th.rs
@@ -1,9 +1,10 @@
-
-// Fix the error below with least amount of modification to the code
 fn main() {
-    let x: i32; // Uninitialized but used, ERROR !
-    let y: i32; // Uninitialized but also unused, only a Warning !
+    let _x: i32; // Uninitialized but used, ERROR !
+    let _y: i32; // Uninitialized but also unused, only a Warning !
 
     // assert_eq!(x, 5);
     println!("Success!");
 }
+
+
+// By prefixing the variable name with an underscore, you're essentially saying, "Yes, I know I'm not using this variable, and it's intentional. Please don't warn me about it."
