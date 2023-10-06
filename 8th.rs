76fcfc799bfd92b2f9f6dde20b7e6bbbf2705ef8
whoami-anoint <abhishekkfl@gmail.com>
2023-10-06 22:45:21 +0530

    update files

--- 8th.rs
+++ 8th.rs
@@ -1,7 +1,10 @@
 fn main() {
-    let _x: i32; // Uninitialized but used, ERROR !
-    let _y: i32; // Uninitialized but also unused, only a Warning !
+    // let x: i32; // Uninitialized but used, ERROR !
+    // let y: i32; // Uninitialized but also unused, only a Warning !
 
+
+    let _x: i32; 
+    let _y: i32; 
     // assert_eq!(x, 5);
     println!("Success!");
 }
