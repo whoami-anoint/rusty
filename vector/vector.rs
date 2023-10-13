3754e0b76091cf6e3a9c8103c4a7597437b21d14
whoami-anoint <abhishekkfl@gmail.com>
2023-10-13 22:20:12 +0530

    update vector

---
+++ vector/vector.rs
@@ -0,0 +1,12 @@
+// fn main(){
+//     let v = vec![5,6,7];
+//     println!("{:?}",v[0]);
+//     let mut v = vec![5,6,7];
+//     v.push(8);
+//     println!("{:?}",v);
+// }
+
+fn main(){
+    let v = vec![11,12,13,14];
+    println!("{:?}",v);
+}
